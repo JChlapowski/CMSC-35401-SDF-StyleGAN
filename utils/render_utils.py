09d07cab423daad5dba88bf32479c257bc278dee
JChlapowski <jchlapowski@uchicago.edu>
2023-01-23 16:57:23 -0600

    Changing filepaths to match more appropriately

--- utils/render_utils.py
+++ utils/render_utils.py
@@ -14,7 +14,7 @@ def generate_image_for_fid(mesh, mydir, i = 0):
   render_resolution = 299
   mesh = scale_to_unit_sphere(mesh)
   for j in range(20):
-    ensure_directory(f"{mydir}/view_{j}")
+    ensure_directory(f"{mydir}")
     image = render_mesh(mesh, index=j, resolution=render_resolution)/255
     torchvision.utils.save_image(torch.from_numpy(image.copy()).permute(
-        2, 0, 1), f"{mydir}/view_{j}/{i}.png")
+        2, 0, 1), f"{mydir}/{j}.png")
